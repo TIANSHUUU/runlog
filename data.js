387ceceb812fa3ed190eb977a392de55cc6ff064
alubiaspinto <tianshu.tan@gmail.com>
2026-04-05 12:50:13 +1000

    Swap Gardiners Creek photo order

--- data.js
+++ data.js
@@ -198,8 +198,8 @@ const ROUTES = [
       }
     ],
     photos: [
-      "images/gardiners-creek-trail/01.jpg",
-      "images/gardiners-creek-trail/thumb.jpg"
+      "images/gardiners-creek-trail/thumb.jpg",
+      "images/gardiners-creek-trail/01.jpg"
     ],
     nearby: [],
     coordinates: [
